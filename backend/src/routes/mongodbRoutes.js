d7ba3354637214f32660264481c3f3365d0374eb
Muhammad Noman Khan <muhammadnomankhan482@gmail.com>
2026-06-05 17:47:45 +0500

    sync: app.js and mongodbRoutes.js from e-commerce-17 instructor repo

--- backend/src/routes/mongodbRoutes.js
+++ backend/src/routes/mongodbRoutes.js
@@ -1,68 +1,62 @@
 const mongoose = require('mongoose')
-const { applyTimestamps } = require('../models/Product')
 const express = require("express")
 
-const rout = express.Router
+const router = express.Router()
 
-const userSchema = mongosse.schema({
-    userId:{
+const userSchema = mongoose.Schema({
+    userId: {
         type: Number,
         unique: true,
         required: true
     },
-    firstName:{
+    firstName: {
         type: String,
         required: true
     },
-    lastName:{
+    lastName: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password:{
+    passwordHash: {
         type: String,
         required: true
     },
-    socialMedia:{
+    socialMedia: {
         type: Array,
-
     },
-}
 
+
+},
     {
-        timesta
+        timestamps: true,
     }
-
 )
 
+const User = mongoose.model("User" , userSchema)
 
-const User = mongoose.model("User" , "userSchema")
 
+router.get("/" , async (req , res) =>{
+  const users =  await User.find()
 
-router.get("/", async (req , res) =>{
-    const users = await User.find()
-    users.length > 0 ? res.status(200).json(users) : res.status(404).json({message: "No users found"})
+  res.json(users).statusCode(200)
 })
+
 
 router.post("/" , async (req , res) =>{
     const user = User.create({
-        userId:1,
-        firstName:"John",
-        lastName:"Doe",
-        email:"john.doe@example.com",
-        password:"password123"
-        
+        userId: 5,
+        firstName: 'Arham',
+        lastName: 'Ali',
+        email: 'arhamali12345@gmail.com',
+        passwordHash: '1234567890',
     })
 
-    res.json("user created successfully", {...user, password:"nahi bataonga"
-    })
-    const {userId , firstName , lastName , email , password} = req.body
-    const user = new User({userId , firstName , lastName , email , password})
-    await user.save()
-    res.status(201).json(user)
+    res.status(201).json({message: '"user created successfully!" ' , ...user, passwordHash: 'nh btaunga'})
 })
+
 module.exports = router
